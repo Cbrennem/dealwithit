2f25ba3a5576776bdf0d89e0d475c176ecaffa33
Chris <Cbrennem82@gmail.com>
2016-03-06 18:09:36 +0900

    Added Toolbar After reSync

--- app/src/main/java/com/example/threegnome/dealwithit/MainActivity.java
+++ app/src/main/java/com/example/threegnome/dealwithit/MainActivity.java
@@ -28,6 +28,7 @@ import android.os.Build;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -83,6 +84,9 @@ public class MainActivity extends AppCompatActivity  {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.topToolbar);
+        setSupportActionBar(toolbar);
 
         imgviewPhoto = (ImageView) findViewById(R.id.photoView);
 
